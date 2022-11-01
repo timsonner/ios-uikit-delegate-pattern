c79f28594af3988a99d1e5b312350133f9c6f3fe
timsonner <66705347+timsonner@users.noreply.github.com>
2022-11-01 15:02:11 -0600

    Initial Commit

---
+++ Delegate Pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delegate Pattern
+//
+//  Created by Timothy Sonner on 11/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
